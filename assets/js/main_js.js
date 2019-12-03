96879aeacd64669e4c95e411a5f0f47185a64073
innerfly <v.sukhoff@csgo.com>
2019-12-03 14:23:42 +0300

    carousel

---
+++ assets/js/main_js.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay: true,
+        smartSpeed: 6000,
+        autoplayTimeout: 6000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+})
