db388069a40a0d012e4f682f11d90219897d73d1
ellavrubel <webella@protonmail.com>
2019-12-16 15:01:15 +0300

    ajax

---
+++ assets/js/form_mail.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    $('#sendmail').on('click', function () {
+            var email = $('#EMAIL').val().trim();
+            var name = $('#NAME').val().trim();
+            var message = $('#MSG').val().trim();
+
+            if (email === "") {
+                $('#errorMess').text('Введите email');
+
+                return false;
+            } else if (name === "") {
+                $('#errorMess').text('Введите имя/наименование организации');
+
+                return false;
+            } else if (message.length < 5){
+                $('#errorMess').text('Напишите чуть больще по сути вашего обращения');
+                return false;
+            }
+
+            $('#errorMess').text('');
+
+
+
+
+    });
+
+})
