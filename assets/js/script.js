e439266ebb676eea046666d198c4805f99dd105d
ellavrubel <webella@protonmail.com>
2020-04-09 12:17:52 +0300

    adaptive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-  $(function () {
-
-
-
-
-
-
-
-
-
-  });
+// $(document).ready(function () {
+//
+//   $('.menu__button-wrapper').click(function (e) {
+//
+//     e.preventDefault();
+//
+//     $('.header__nav').toggleClass('active');
+//     // $('body').toggleClass('lock');
+//
+//   })
+//
+// });
